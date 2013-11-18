6b34b0512d79a7dd58dfe37de6d4563ed5c97d8d
Matt Campbell <mecampbell25@gmail.com>
2013-11-18 17:25:15 -0500

    Fix render to redirect_to after success

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,7 @@ class MessagesController < ApplicationController
   def create
     @message = Message.create(message_params)
     if @message.send!
-      render :new
+      redirect_to new_message_path
     end
   end
 
