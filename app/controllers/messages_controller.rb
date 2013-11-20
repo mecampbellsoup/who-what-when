8911ac84abd9b01adc57043f12a57552a5ad5bc5
trevmckendrick <trevormckendrick@gmail.com>
2013-11-20 10:12:48 -0500

    refactored routes and messages controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -6,17 +6,17 @@ class MessagesController < ApplicationController
     @message = Message.new
   end
 
-  def create
+  def create_from_web
     @receiver = Receiver.find_or_create_by(:phone => format_phone_number(params[:message][:receiver]))
     @message = @receiver.messages.create(message_params)
     TwilioWorker.perform_at(@message.send_at, @message.id)
     redirect_to root_path
   end
 
-  def create_from_reply
+  def create_from_sms
     @message = Message.create_from_sms(params)
     TwilioWorker.perform_at(@message.send_at, @message.id)
-    render :nothing
+    render :nothing => true
   end
 
 
