d15cfdcb3acf68657b8fe6dec27651699e1e86c5
Scott Luptowski <scottluptowski@gmail.com>
2013-11-20 09:59:15 -0500

    refactor

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,12 +2,10 @@ class Message < ActiveRecord::Base
   belongs_to :receiver
 
   def send_at=(date)
-    if date.is_a?(Time)
-      write_attribute :send_at, date
-    else
+    if !date.is_a?(Time)
       date = Chronic.parse(date) || Time.now
-      write_attribute :send_at, date
     end
+    write_attribute :send_at, date
   end
 
 end
