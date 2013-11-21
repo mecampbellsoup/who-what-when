2082006faac877cb498f94c319837ccb3bfae662
trevmckendrick <trevormckendrick@gmail.com>
2013-11-21 16:51:38 -0500

    refactoring

--- app/models/concerns/textable.rb
+++ app/models/concerns/textable.rb
@@ -3,32 +3,25 @@ module Textable
   TimeKeywords = ["in", "on", "when", "until"]
 
   def locate_time_keyword(sentence)
-    TimeKeywords.each do |w|
+
+    keyword = TimeKeywords.each do |w|
       next if !sentence.index(/\s#{w}\s/)
-      return sentence[sentence.index(w), w.length]
+      return sentence[sentence.index(w), w.length] 
     end
+
+    false if keyword.is_a?(Array) 
   end
 
   def parse_text_body_at_keyword(sentence)
-    if keyword_found?(sentence) 
+    if locate_time_keyword(sentence) 
       sentence.split(locate_time_keyword(sentence)).first.strip
     else
       nil
     end
   end
 
-  def keyword_found?(sentence)
-    sentence_array = sentence.split(" ")
-    if TimeKeywords.each { |keyword| break true if sentence_array.include?(keyword) } == true
-      return true
-    else
-      false
-    end
-
-  end
-
   def parse_text_time_at_keyword(sentence)
-    if keyword_found?(sentence) 
+    if locate_time_keyword(sentence) 
       Chronic.parse("#{locate_time_keyword(sentence)} #{sentence.split(locate_time_keyword(sentence)).last.strip}")
     else
       nil
