fd2a336563170d5d2b54871ab4b43e530bfc1497
Matt Campbell <mecampbell25@gmail.com>
2013-11-22 13:06:38 -0500

    fixing feature spec

--- spec/features/messages_spec.rb
+++ spec/features/messages_spec.rb
@@ -2,15 +2,16 @@ require 'spec_helper'
 
 describe "sending a text" do 
 
-  it "sends a text" do 
-    visit '/'
-    within("#duck-form") do 
-      fill_in '#phone_input', :with => ''
-      fill_in '#message_body', :with => 'Remind me about the game'
-      fill_in '#message_send_at', :with => 'now'
+  it "sends a text" do
+    visit root_path
+    within("form") do 
+      fill_in "phone_input", :with => '(740)319-0208'
+      fill_in 'message_body', :with => 'Remind me about the game'
+      fill_in 'message_send_at', :with => 'now'
     end
-    binding.pry
-    click_link '#submit_button'
+    expect{
+      click_button 'submit_button'
+    }.to change { Message.count }.by(1)
   end
 
 end
