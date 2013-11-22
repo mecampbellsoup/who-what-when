44934a3774a510767432b71520bd8dfdb1dc65b5
Matt Campbell <mecampbell25@gmail.com>
2013-11-22 13:07:36 -0500

    updating feature spec

--- spec/features/messages_spec.rb
+++ spec/features/messages_spec.rb
@@ -2,16 +2,28 @@ require 'spec_helper'
 
 describe "sending a text" do 
 
-  it "sends a text" do
+  it "sends a text" do 
     visit root_path
-    within("form") do 
-      fill_in "phone_input", :with => '(740)319-0208'
+
+      fill_in 'phone_input', :with => '6262444636'
       fill_in 'message_body', :with => 'Remind me about the game'
       fill_in 'message_send_at', :with => 'now'
-    end
-    expect{
-      click_button 'submit_button'
-    }.to change { Message.count }.by(1)
+      
+      expect{
+        click_button 'submit_button'
+      }.to change { Message.count }.by(1)
+  end
+
+  it "shouldn't send a text with bad user data" do 
+    visit root_path
+
+      fill_in 'phone_input', :with => '2444636'
+      fill_in 'message_body', :with => 'Remind me about the game'
+      fill_in 'message_send_at', :with => 'now'
+      
+      expect{
+        click_button 'submit_button'
+      }.to raise_exception ActiveRecord::RecordNotSaved
   end
 
 end
