982f6bd17fe1f85bdd6404d634a7537f590c0253
Matt Campbell <mecampbell25@gmail.com>
2013-11-22 14:37:18 -0500

    removing sms_time_validators

--- app/models/sms_time_validator.rb
+++
@@ -1,8 +0,0 @@
-class SmsTimeValidator < ActiveModel::Validator
-
-  def validate(record)
-    if record.send_at.class != Time
-      record.errors[:send_at] << "Hello! You didn't send us a time. Please send again."
-    end
-  end
-end
--- db/migrate/20131120231745_create_sms_time_validators.rb
+++
@@ -1,8 +0,0 @@
-class CreateSmsTimeValidators < ActiveRecord::Migration
-  def change
-    create_table :sms_time_validators do |t|
-
-      t.timestamps
-    end
-  end
-end
